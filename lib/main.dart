b160083f131c0f4e8ebcd43f94a4f7fb47fc818c
suvankar-mitra <16486734+suvankar-mitra@users.noreply.github.com>
2023-12-17 01:26:11 +0530

    Added light and dark theme switcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:cool_calculator/screen/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDark = false;
+
+  @override
+  void initState() {
+    var brightness =
+        SchedulerBinding.instance.platformDispatcher.platformBrightness;
+    _isDark = brightness == Brightness.dark;
+    super.initState();
+  }
 
   // This widget is the root of your application.
   @override
@@ -15,10 +31,34 @@ class MyApp extends StatelessWidget {
       title: 'Cool Calculator',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        brightness: Brightness.light,
+        /* light theme settings */
+        colorScheme: ColorScheme.light(
+          primary: Colors.grey.shade800,
+          primaryContainer: Colors.grey.shade50,
+        ),
         useMaterial3: true,
       ),
-      home: const Home(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        /* dark theme settings */
+        colorScheme: ColorScheme.dark(
+          primary: Colors.grey.shade600,
+          primaryContainer: Colors.grey.shade900,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: _isDark ? ThemeMode.dark : ThemeMode.light,
+      home: Home(
+        switchLightDarkMode: _switchLightDarkMode,
+        isDarkModeInit: _isDark,
+      ),
     );
+  }
+
+  void _switchLightDarkMode(bool isDark) {
+    setState(() {
+      _isDark = isDark;
+    });
   }
 }
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends StatefulWidget {
+  final dynamic switchLightDarkMode;
+  final bool isDarkModeInit;
+  const Home(
+      {super.key,
+      required this.switchLightDarkMode,
+      required this.isDarkModeInit});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
+    bool isDarkMode = widget.isDarkModeInit;
     return Scaffold(
       appBar: AppBar(
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              child: Row(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Icon(Icons.light_mode,
-                        color: Theme.of(context).colorScheme.primary),
-                  ),
-                  Container(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Icon(Icons.dark_mode,
-                          color: Theme.of(context).colorScheme.secondary)),
-                ],
-              ),
-            )
+              margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isDarkMode = !isDarkMode;
+                    });
+                    widget.switchLightDarkMode(isDarkMode);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      elevation: 2,
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(0),
+                      backgroundColor:
+                          Theme.of(context).colorScheme.primaryContainer),
+                  child: Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode,
+                      color: Theme.of(context).colorScheme.primary)),
+            ),
           ],
         ),
       ),
